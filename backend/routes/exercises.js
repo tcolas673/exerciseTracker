30c81196f51640bae703538a455b5a558c96213e
tcolas673 <tcolas673@gmail.com>
2020-01-19 09:12:31 -0500

    rebasing shit

---
+++ backend/routes/exercises.js
@@ -0,0 +1,7 @@
+const UserRouter = require('express').Router();
+let User = require('../models/user.model');
+
+UserRouter.route()
+
+
+module.exports = UserRouter;
---
+++ backend/server.js
@@ -0,0 +1,39 @@
+// // const express = require('express');
+// // const cors = require('cors');
+// // const mongoose = require('mongoose');
+
+// // require('dotenv').config();
+
+// // const app = express();
+// // const port = 5000;
+
+// // const uri = process.env.ATLAS_URI;
+// // mongoose.connect(uri, {useNewUrlParser:true, useCreateIndex:true});
+
+// // // app.use(cors());
+// // // app.use(express.json());
+// // const exerciseRouter = require('./routes/exercises');
+// // const userRouter = require('./routes/users');
+
+// // app.use('/exercises',exerciseRouter );
+// // app.use('/users', userRouter);
+
+// // app.listen(port, ()=>{
+// //     console.log('Server is running on port:'+port);
+// // })
+
+// // const app = (require('express'))().get('/',
+// // (err,req,res)=>{ return 'Hello Server';}).listen(3000);
+
+// console.log(Number('1'+'')===1|| NaN);
+// const success = {message: 'yeh'};
+// success.age =1;
+// console.log(success);
+// let a = [1,2,3];
+// console.log(a.slice(1));
+// console.log(a);
+
+let a = new Set();
+a.add(1);
+a.add('w');
+console.log(a);
